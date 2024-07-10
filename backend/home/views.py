18cc5e6b670e554671324ac4281115b37ac8585f
rena02to <rena02to@gmail.com>
2024-07-10 10:47:26 -0300

    testando a pagina da rota inicial do projeto

--- backend/home/views.py
+++ backend/home/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Here will be the project homepage.')
+    return render(request, 'home/home.html')
