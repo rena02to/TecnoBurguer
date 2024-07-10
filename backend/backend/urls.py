3f07d2d35f0a17fd2e15a498d9e74901d05eeb05
rena02to <rena02to@gmail.com>
2024-07-10 11:39:24 -0300

    tentativa de personalizar o icone para todas as páginas

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.ico', lambda _ : redirect('static/images/favicon.ico', permanent=True)),
     path('', include('home.urls')),
 ]
