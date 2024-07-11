e881272dd6b4135ada1915c6820374bfa9d52ef4
rena02to <rena02to@gmail.com>
2024-07-11 12:08:22 -0300

    inicio das personalizações iniciais do frontend

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login | TecnoBurguer"
+};
+
+export default function Login(){
+    return(
+        <></>
+    );
+}
