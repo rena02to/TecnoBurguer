435c4c35f9e98fc1c6eaf3f1d48cf2d3076fce20
Renato Alves <rena02to@gmail.com>
2024-08-15 16:21:15 -0300

    correction in the way media files are served in deploy

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.shortcuts import redirect
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 
 urlpatterns = [
@@ -10,4 +11,11 @@ urlpatterns = [
     path('favicon.ico', lambda _ : redirect('static/images/favicon.ico', permanent=True)),
     path('', include('home.urls')),
     path('api/', include('api.urls')),
+    re_path(
+            r"^media/(?P<path>.*)$",
+            serve,
+            {
+                "document_root": settings.MEDIA_ROOT,
+            },
+        ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
