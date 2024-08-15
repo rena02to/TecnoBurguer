71a21dd38ee0d2b00884f7327bfbddbfcecaaa31
Renato Alves <rena02to@gmail.com>
2024-08-15 14:41:33 -0300

    correction in the way media files are served in deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -150,7 +150,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIAFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 
