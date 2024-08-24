4d34336767244dc2050d4d7450102d355c5d253d
Renato Alves <rena02to@gmail.com>
2024-08-24 11:04:52 -0300

    creating error 404 page

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -1,9 +1,21 @@
+import Footer from '@/components/Footer';
+import style from './styles/not-found.module.scss';
+import Image from 'next/image';
+
 export const metadata = {
     title: '404 Error | TecnoBurguer'
 };
 
 export default function Custom404() {
     return(
-        <></>
+        <div className={style.notFound}>
+            <main>
+                <h1><span>OOPS...</span> Parece que esta página não existe!</h1>
+                <p>Confira se você não digitou algo errado</p>
+                <p>Talvez o endereço não exista mais na web</p>
+                <Image src='/Images/404.png' width={468} height={275} alt='404 Image'/>
+            </main>
+            <Footer/>
+        </div>
     )
 }
