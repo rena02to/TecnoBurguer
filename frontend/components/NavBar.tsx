9475a7bc7dfee3508a06a933bb7ce5962792a22d
rena02to <rena02to@gmail.com>
2024-09-04 01:06:46 -0300

    Inserting navbar in project (partial)

--- frontend/components/NavBar.tsx
+++ frontend/components/NavBar.tsx
@@ -4,22 +4,27 @@ import style from './styles/navbar.module.scss';
 import Image from 'next/image';
 import { IoMenu } from 'react-icons/io5';
 import { FaHome } from 'react-icons/fa';
+import { useContexts } from '@/context/context';
+import { useTranslations } from 'use-intl';
 
 type Link = {
-  key: number;
-  title: string;
-  link: string;
-  icon: JSX.Element;
+    key: number;
+    title: string;
+    link: string;
+    icon: JSX.Element | null;
 };
 
 type Links = {
-  client: Link[];
-  admin: Link[];
+    client: Link[];
+    admin: Link[];
+    loggedout: Link[];
 };
 
 export default function NavBar() {
     const [menuOpen, setMenuOpen] = useState(false);
-    const [typeUser, setTypeUser] = useState<keyof Links>('client');
+    const [typeUser, setTypeUser] = useState<keyof Links>("loggedout");
+    const { type } = useContexts();
+    const t = useTranslations('Navbar');
 
     const links: Links = {
         client: [
@@ -34,9 +39,17 @@ export default function NavBar() {
             { key: 3, title: 'Cupons', link: '/', icon: <FaHome /> },
             { key: 4, title: 'Perfil', link: '/', icon: <FaHome /> },
         ],
+        loggedout: [
+            { key: 1, title: 'Login', link: '/login', icon: null },
+            { key: 2, title: t('signup'), link: '/', icon: null },
+        ],
     };
 
-    useEffect(() => {}, []);
+    useEffect(() => {
+        if(type === 'admin' || type === 'client' || type === 'loggedout'){
+            setTypeUser(type);
+        }
+    }, [ type ]);
 
     return (
         <nav className={menuOpen ? style.openNavbar : style.closeNavbar}>
